428a1582fd1d647c484cd0e98ffccad3ff3d30e3
Mjdixon <mjdixon@brooksschool.org>
2026-02-11 10:30:14 -0500

    added players got rid of hands and did scanners

--- Dealer.java
+++ Dealer.java
@@ -2,12 +2,11 @@ import java.util.Arrays;
 
 public class Dealer {
 
-    // private Player[] players = new Player[10];
     private int deck[] = new int[55];
     private int score;
 
-    public Dealer() {
-        score = (int) (13);
+    public Dealer(int people) {
+        score = (int) (60 / people + 1);
         int indx = 0;
         for (int i = 0; i <= 10; i++) {
             for (int j = 0; j < i; j++) {
@@ -20,4 +19,13 @@ public class Dealer {
     public void showDeck() {
         System.out.println(Arrays.toString(deck));
     }
+
+    public void setPlayers() {
+        for (int i = 0; i < players.length; i++) {
+            players[i] = i;
+        }
+        System.out.println(Arrays.toString((players)));
+
+    }
+
 }
--- PiratePairs.java
+++ PiratePairs.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 public class PiratePairs {
     public static void main(String[] args) {
         // System.out.println(.Arrays);
+        Player[] players = new Player[10];
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many players do you want?");
         int people = scanner.nextInt();
-        System.out.println("What is the names?");
-        String names = scanner.nextLine();
-        System.out.println("Players: " + names);
+        // System.out.println("What is the names?");
+        // String name = scanner.next();
+        // System.out.println("Players: " + name);
         System.out.println("Amount of Players: " + people);
         scanner.close();
-        System.out.println("hello wghfghfuorld");
 
-        Dealer dealer = new Dealer();
+        Dealer dealer = new Dealer(people);
         dealer.showDeck();
+        Deck deck = new Deck();
 
     }
 }
