16bc04c756c60427ee33d5b92a156173976cb8ec
Mjdixon <mjdixon@brooksschool.org>
2026-02-16 10:02:42 -0500

    added checkHand, fixed takeCard, added player scanner and current player, added comments and fixed a couple logic errors

--- PiratePairs.java
+++ PiratePairs.java
@@ -6,19 +6,33 @@ public class PiratePairs {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many players do you want?");
         int people = scanner.nextInt();
-        // 5Players[] players = new Players[people];
         System.out.println("Amount of Players: " + people);
+        Player[] players = new Player[people];
+        for(int i = 0; i<people;i++){
+            System.out.println("Enter Names ");
+            String name = scanner.nextLine();
+            players[i] = new Player(name);
+        }
         // Sets new dealer 
         Dealer dealer = new Dealer(people);
         Player player = new Player(people);
         dealer.showDeck();
+        //Shuffles
         dealer.shuffle();
         dealer.showDeck();
-        System.out.println("Do you want to draw?");
+        Player currentPlayer = players[0];
+        //Draw Cards
         int temp = dealer.drawCard();
         System.out.println(temp);
         dealer.showDeck();
-        player.takeCard(temp);
+        currentPlayer.takeCard(temp);
+        player.showHand();
+        //Take Card
+        dealer.drawCard();
+        currentPlayer.takeCard(temp);
+        currentPlayer.showHand();
+        currentPlayer.checkHand();
+        
         scanner.close();
     }
 }
--- Player.java
+++ Player.java
@@ -1,24 +1,43 @@
+import java.util.Arrays;
+
 public class Player {
     private int score;
+    private int playerScore;
     private int[] hand = new int[0];
+    //Initializes player
     public Player(int people) {
         score = (int) (60 / people + 1);
-        int idvScore = 0;
         int points = 0;
     }
 
-   
-    public int getPoints(int idv, int points) {
-
-        return points;
-    }
-
+  
+    //Grabs Score
     public int getScore() {
         return score;
     }
+    //Grabs card from Deck
     public void takeCard(int card){
-        int temp = hand[card];
-        temp = hand[card];
+        int[] newHand = new int[hand.length +1];
+        for(int i = 0; i<hand.length;i++){
+            newHand[i] = hand[i];
+        }
+        newHand[newHand.length-1] = card;
+        hand = newHand;
+    }//Shows Hand
+    public void showHand(){
+        System.out.println(Arrays.toString(hand));
+    }//checks hand for score
+    public void checkHand(){
+        int lastCard = hand[hand.length-2];
+        int currentCard = hand[hand.length-1];
+        if(hand.length>=2){
+            if(lastCard == currentCard){
+                playerScore += currentCard;
+            }
+            else{
+                playerScore += 0;
+            }
+        }
+        System.out.println(playerScore);
     }
-    
 }
